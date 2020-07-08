dd340f813aa51d560e217f942f139ccb1fedc822
Nguyen Tran <nguyen.tran2092@gmail.com>
2020-07-08 09:31:22 +0700

    update

--- backend/modules/news/src/migration/m200708_020618_create_table_news.php
+++ backend/modules/news/src/migration/m200708_020618_create_table_news.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m200704_040528_create_table_news_category
+ * Class m200708_020618_create_table_news
  */
-class m200704_040528_create_table_news_category extends Migration
+class m200708_020618_create_table_news extends Migration
 {
     /**
      * {@inheritdoc}
@@ -18,12 +18,13 @@ class m200704_040528_create_table_news_category extends Migration
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
         }
-        $check_table = Yii::$app->db->getTableSchema('news_category');
+        $check_table = Yii::$app->db->getTableSchema('news');
         if ($check_table === null) {
-            $this->createTable('news_category', [
+            $this->createTable('news', [
                 'id' => $this->primaryKey(),
-                'category' => $this->integer(11)->null()->defaultValue(0),
                 'slug' => $this->string(255)->notNull(),
+                'category' => $this->integer(11)->null()->defaultValue(0),
+                'type' => $this->integer(11)->null()->defaultValue(0)->comment('0: Tin tức, 1: Dự án,...'),
                 'image' => $this->string(255)->null(),
                 'status' => $this->tinyInteger(1)->null()->defaultValue(1),
                 'sort' => $this->integer(11)->null()->defaultValue(1)->comment('Thứ tự'),
@@ -33,26 +34,27 @@ class m200704_040528_create_table_news_category extends Migration
                 'updated_by' => $this->integer(11)->null()->defaultValue(1),
                 'alias' => $this->string(500)->null(),
             ], $tableOptions);
-            $this->addForeignKey('news_category-category-news_category-id', 'news_category', 'category', 'news_category', 'id', 'RESTRICT', 'CASCADE');
-            $this->addForeignKey('news_category-created_by-user-id', 'news_category', 'created_by', 'user', 'id', 'RESTRICT', 'CASCADE');
-            $this->addForeignKey('news_category-updated_by-user-id', 'news_category', 'updated_by', 'user', 'id', 'RESTRICT', 'CASCADE');
+            $this->addForeignKey('news-category-news_category-id', 'news', 'category', 'news_category', 'id', 'RESTRICT', 'CASCADE');
+            $this->addForeignKey('news-created_by-user-id', 'news', 'created_by', 'user', 'id', 'RESTRICT', 'CASCADE');
+            $this->addForeignKey('news-updated_by-user-id', 'news', 'updated_by', 'user', 'id', 'RESTRICT', 'CASCADE');
         }
-        if (!is_dir(Yii::getAlias('@frontend/web/uploads/news-category'))) {
-            @mkdir(Yii::getAlias('@frontend/web/uploads/news-category'), 0775, true);
+        if (!is_dir(Yii::getAlias('@frontend/web/uploads/news'))) {
+            @mkdir(Yii::getAlias('@frontend/web/uploads/news'), 0775, true);
         }
-        $check_table_language = Yii::$app->db->getTableSchema('news_category_language');
+        $check_table_language = Yii::$app->db->getTableSchema('news_language');
         if ($check_table_language === null) {
-            $this->createTable('news_category_language', [
-                'news_category_id' => $this->integer(11)->notNull(),
+            $this->createTable('news_language', [
+                'news_id' => $this->integer(11)->notNull(),
                 'language_id' => $this->integer(11)->notNull(),
                 'name' => $this->string(255)->notNull(),
                 'slug' => $this->string(255)->notNull(),
                 'description' => $this->text()->null(),
+                'content' => $this->text()->null(),
                 'metadata' => $this->json()->null()
             ], $tableOptions);
-            $this->addPrimaryKey('news_category_language-category_id-language_id', 'news_category_language', ['news_category_id', 'language_id']);
-            $this->addForeignKey('news_category_language-news_category_id-news_category-id', 'news_category_language', 'news_category_id', 'news_category', 'id', 'CASCADE', 'CASCADE');
-            $this->addForeignKey('news_category_language-language_id-language-id', 'news_category_language', 'language_id', 'language', 'id', 'CASCADE', 'CASCADE');
+            $this->addPrimaryKey('news_language-news_id-language_id', 'news_language', ['news_id', 'language_id']);
+            $this->addForeignKey('news_language-news_id-news-id', 'news_language', 'news_id', 'news', 'id', 'CASCADE', 'CASCADE');
+            $this->addForeignKey('news_language-language_id-language-id', 'news_language', 'language_id', 'language', 'id', 'CASCADE', 'CASCADE');
         }
     }
 
@@ -61,7 +63,7 @@ class m200704_040528_create_table_news_category extends Migration
      */
     public function safeDown()
     {
-        echo "m200704_040528_create_table_news_category cannot be reverted.\n";
+        echo "m200708_020618_create_table_news cannot be reverted.\n";
 
         return false;
     }
@@ -75,7 +77,7 @@ class m200704_040528_create_table_news_category extends Migration
 
     public function down()
     {
-        echo "m200704_040528_create_table_news_category cannot be reverted.\n";
+        echo "m200708_020618_create_table_news cannot be reverted.\n";
 
         return false;
     }
