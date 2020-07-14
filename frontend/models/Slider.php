77be16d47699e370710015b3e1a442836e6cce0b
Tam Nguyen <tam.nguyen@365dep.vn>
2020-07-14 14:19:10 +0700

    create slider

---
+++ frontend/models/Slider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luken
+ * Date: 7/14/2020
+ * Time: 11:45
+ */
+
+namespace frontend\models;
+
+
+use milkyway\slider\models\table\SliderTable;
+
+class Slider extends SliderTable
+{
+
+}
