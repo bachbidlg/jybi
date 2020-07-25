cb46550cadf6af9e147ee54ae2ac511e53185db0
Tam Nguyen <tam.nguyen@365dep.vn>
2020-07-25 09:46:08 +0700

    update news n project index

--- frontend/views/news/p-index.php
+++ frontend/views/news/p-index.php
@@ -10,24 +10,80 @@ $this->title = $category->newsCategoryLanguage[$default_language]->name;
 \Yii::$app->view->params['breadcrumbs'][] = $this->title;
 ?>
 <section class="page-projects">
-    <div class="container">
+    <div class="container-fluid">
         <div id="list-projects">
-            <div id="filters-masonry" class="work-filter">
+            <div id="filters-masonry" class="filters filter-button-group">
                 <div data-filter="*" class="filter-item filter-item-active">All</div>
-                <div data-filter=".cate-1" class="filter-item filter-item-active">Project Category 1</div>
-                <div data-filter=".cate-2" class="filter-item">Project Category 2</div>
-                <div data-filter=".cate-3" class="filter-item">Project Category 3</div>
+                <div data-filter=".nha-pho" class="filter-item">Nhà phố</div>
+                <div data-filter=".biet-thu" class="filter-item">Biệt thự</div>
+                <div data-filter=".van-phong" class="filter-item">Văn phòng</div>
             </div>
-            <div id="grid-masonry" class="cbp">
-                <div class="cbp-item cate-1">
-                    <a href="#">
-                        <div class="image-wrap">
-                            <img src="" alt="">
+            <div id="grid-masonry" class="grid">
+                <div class="row row-cols-md-3 row-cols-2" style="margin:0 -.5rem">
+                    <?php for ($i = 1; $i <= 4; $i++) { ?>
+                        <div class="col grid-item nha-pho mb-3 px-2">
+                            <a class="caption" href="#">
+                                <div class="image-wrap">
+                                    <img class="img-fluid" src="./images/image<?= $i ?>.jpg" alt="img">
+                                </div>
+                                <div class="caption-wrap">
+                                    <div class="d-table">
+                                        <div class="d-table-cell text-center">
+                                            <div class="title">Nhà phố</div>
+                                            <div class="line"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                        <div class="caption-wrap">
-                            <div class="title"></div>
+                    <?php } ?>
+                    <?php for ($i = 5; $i <= 8; $i++) { ?>
+                        <div class="col grid-item biet-thu mb-3 px-2">
+                            <a class="caption" href="#">
+                                <div class="image-wrap">
+                                    <img class="img-fluid" src="./images/image<?= $i ?>.jpg" alt="img">
+                                </div>
+                                <div class="caption-wrap">
+                                    <div class="d-table">
+                                        <div class="d-table-cell text-center">
+                                            <div class="title">Biệt thự</div>
+                                            <div class="line"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                    <?php } ?>
+                    <div class="col grid-item van-phong mb-3 px-2">
+                        <a class="caption" href="#">
+                            <div class="image-wrap">
+                                <img class="img-fluid" src="./images/image9.jpg" alt="img">
+                            </div>
+                            <div class="caption-wrap">
+                                <div class="d-table">
+                                    <div class="d-table-cell text-center">
+                                        <div class="title">Văn phòng</div>
+                                        <div class="line"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col grid-item van-phong mb-3 px-2">
+                        <a class="caption" href="#">
+                            <div class="image-wrap">
+                                <img class="img-fluid" src="./images/image10.jpg" alt="img">
+                            </div>
+                            <div class="caption-wrap">
+                                <div class="d-table">
+                                    <div class="d-table-cell text-center">
+                                        <div class="title">Văn phòng</div>
+                                        <div class="line"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,10 +91,20 @@ $this->title = $category->newsCategoryLanguage[$default_language]->name;
 </section>
 <?php
 $script = <<< JS
-$('.masonry').masonry({
-    itemSelector: '.msr-item',
+$(function () {
+    $('.grid').isotope({
+      itemSelector: '.grid-item',
+    });
+    
+    // filter items on button click
+    $('.filter-button-group').on( 'click', '.filter-item', function() {
+        var filterValue = $(this).attr('data-filter');
+        $('.grid').isotope({ filter: filterValue });
+        $('.filter-button-group .filter-item').removeClass('filter-item-active');
+        $(this).addClass('filter-item-active');
+    });
 });
 JS;
 $this->registerJs($script, \yii\web\View::POS_END);
-$this->registerJsFile('/js/masonry.pkgd.min.js', ['depends' => 'yii\web\JqueryAsset']);
+$this->registerJsFile('/js/isotope.pkgd.js', ['depends' => 'yii\web\JqueryAsset']);
 ?>
--- frontend/views/news/p-view.php
+++ frontend/views/news/p-view.php
@@ -13,32 +13,34 @@ $this->title = Yii::t('frontend', $news->newsLanguage[$default_language]->name);
 <section class="page-project-detail">
     <div class="container">
         <div class="image-full mb-4">
-            <img src="./images/7.jpg" alt="image" class="img-fluid">
+            <a href="./images/7.jpg" data-fancybox="gallery">
+                <img src="./images/7.jpg" alt="image" class="img-fluid">
+            </a>
         </div>
         <div class="gallery">
             <div class="row row-cols-lg-4 row-cols-md-3 row-cols-2" style="margin:0 -.5rem!important;">
                 <div class="col px-2 mb-3">
-                    <a href="./images/fp1.jpg" title="image" class="msr-item" data-fancybox="gallery">
+                    <a href="./images/fp1.jpg" title="image" class="glr-item" data-fancybox="gallery">
                         <img src="./images/fp1.jpg" alt="image">
                     </a>
                 </div>
                 <div class="col px-2 mb-3">
-                    <a href="./images/fp2.jpg" title="image" class="msr-item" data-fancybox="gallery">
+                    <a href="./images/fp2.jpg" title="image" class="glr-item" data-fancybox="gallery">
                         <img src="./images/fp2.jpg" alt="image">
                     </a>
                 </div>
                 <div class="col px-2 mb-3">
-                    <a href="./images/fp3.jpg" title="image" class="msr-item" data-fancybox="gallery">
+                    <a href="./images/fp3.jpg" title="image" class="glr-item" daota-fancybox="gallery">
                         <img src="./images/fp3.jpg" alt="image">
                     </a>
                 </div>
                 <div class="col px-2 mb-3">
-                    <a href="./images/fp7.jpg" title="image" class="msr-item" data-fancybox="gallery">
+                    <a href="./images/fp7.jpg" title="image" class="glr-item" data-fancybox="gallery">
                         <img src="./images/fp7.jpg" alt="image">
                     </a>
                 </div>
                 <div class="col px-2 mb-3">
-                    <a href="./images/fp8.jpg" title="image" class="msr-item" data-fancybox="gallery">
+                    <a href="./images/fp8.jpg" title="image" class="glr-item" data-fancybox="gallery">
                         <img src="./images/fp8.jpg" alt="image">
                     </a>
                 </div>
@@ -46,12 +48,3 @@ $this->title = Yii::t('frontend', $news->newsLanguage[$default_language]->name);
         </div>
     </div>
 </section>
-<?php
-$script = <<< JS
-// $('.masonry').masonry({
-//     itemSelector: '.msr-item',
-// });
-JS;
-//$this->registerJs($script, \yii\web\View::POS_END);
-$this->registerJsFile('/js/masonry.pkgd.min.js', ['depends' => 'yii\web\JqueryAsset']);
-?>
