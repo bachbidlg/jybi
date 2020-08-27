86cd43ce374fa07adbc6ac1622d836f205afaf94
Nguyen Tran <bachbidlg@gmail.com>
2020-08-27 23:23:52 +0700

    update intro

--- backend/modules/news/src/models/table/NewsTable.php
+++ backend/modules/news/src/models/table/NewsTable.php
@@ -154,12 +154,12 @@ class NewsTable extends \yii\db\ActiveRecord
         return $data;
     }
 
-    public static function getBySlug($slug)
+    public static function getBySlug($slug, $data_cache = YII2_CACHE)
     {
         $cache = Yii::$app->cache;
         $key = 'redis-news-get-by-slug-' . $slug;
         $data = $cache->get($key);
-        if ($data == false) {
+        if ($data == false || $data_cache === false) {
             $query = self::find()->where([self::tableName() . '.slug' => $slug]);
             $data = $query->one();
             $cache->set($key, $data);
